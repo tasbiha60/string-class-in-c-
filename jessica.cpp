c8316247ed1550d0f92270cfbb4fe1f55e2f84c8
tasbiha60 <htjtasbi@gmail.com>
2025-06-14 11:53:23 +0600

    Create jessica.cpp
    if Jessica present then yes
    stringstream

---
+++ jessica.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+
+
+     string s;
+     getline (cin,s);
+
+  stringstream ss(s);
+  string word;
+  while (ss>>word){
+    if(word=="Jessica"){
+        cout<<"YES";
+return 0;
+    }
+  }
+
+
+cout<<"NO";
+
+
+
+
+return 0;
+ }
